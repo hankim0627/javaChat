e2e5739bb0d02e9aa5bb2fcbe411dd4adc4a077c
seoyeong <sunny@naver.com>
2018-02-28 15:40:43 +0900

    ver 3. 닉네임서버전송(클라이언트) 한서영 2018.02.28 15:39

--- step14_network/src/swing/chat/GUIChatClient.java
+++ step14_network/src/swing/chat/GUIChatClient.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -33,6 +37,9 @@ public class GUIChatClient extends JFrame implements ActionListener, Runnable{
 	PrintWriter pw=null;                  //송신스트림
 	BufferedReader br=null;             //수신스트림
 	//테스트1-------------------------------------------------
+	//테스트2-------------------------------------------------
+	OutputStream os=null;
+	//테스트2-------------------------------------------------
 	
 	
 	public GUIChatClient()
@@ -127,8 +134,29 @@ public class GUIChatClient extends JFrame implements ActionListener, Runnable{
 		try{
 			sock=new Socket(ip_txt, PORT);
 			
+			//테스트2(송신)---------------------------------------
+			String nickname=txt_name.getText();
+			os=sock.getOutputStream();
+			pw=new PrintWriter(new OutputStreamWriter(os));
+			pw.println(nickname);                 //주의)pw.print();   -----X
+			pw.flush();			
+			//테스트2(송신)---------------------------------------
+			//테스트3(수신)---------------------------------------
+			InputStream is=sock.getInputStream();
+			br=new BufferedReader(new InputStreamReader(is));
 			
-			
+			String str;
+			while(true)
+			{
+				try{
+					str=br.readLine();
+					txt_list.append(str + "\n");
+				}catch(IOException e){
+					txt_list.append("서버가 종료 됨");
+					return;
+				}
+			}
+			//테스트3(수신)---------------------------------------
 		}catch(IOException e){
 			e.printStackTrace();
 		}		
